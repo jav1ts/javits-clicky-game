56b6795ebe8193fe4e03af596bb0bcdd6c5971c3
jav1ts <55659287+jav1ts@users.noreply.github.com>
2020-05-17 20:16:30 -0400

    Updates

---
+++ precache-manifest.40cc8ff062816129b2895ff7f04a122f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2223bdd9d395ba5de3cb019fc6a3d272",
+    "url": "/javits-clicky-game/index.html"
+  },
+  {
+    "revision": "cea404031b0b2cb01039",
+    "url": "/javits-clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46b0e535e6d2507bbefe",
+    "url": "/javits-clicky-game/static/js/2.2113d3b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javits-clicky-game/static/js/2.2113d3b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea404031b0b2cb01039",
+    "url": "/javits-clicky-game/static/js/main.45e9b162.chunk.js"
+  },
+  {
+    "revision": "5aa2636edca485f02fb2",
+    "url": "/javits-clicky-game/static/js/runtime-main.ccaa6d40.js"
+  },
+  {
+    "revision": "1b8f0876ffda7808cdc0a6a2e0d08665",
+    "url": "/javits-clicky-game/static/media/akuma-3rd.1b8f0876.jpg"
+  },
+  {
+    "revision": "b248fca00fe0d6755eef4185997507bf",
+    "url": "/javits-clicky-game/static/media/alex-3rd.b248fca0.jpg"
+  },
+  {
+    "revision": "c4e5ce6d1edf138679ba8ce226a04298",
+    "url": "/javits-clicky-game/static/media/chunny-3rd.c4e5ce6d.jpg"
+  },
+  {
+    "revision": "a5aafc6eebeae1f7fb4763373b45062b",
+    "url": "/javits-clicky-game/static/media/dudley-3rd.a5aafc6e.jpg"
+  },
+  {
+    "revision": "9326e9c85b17189f4485c95cfc58c6d6",
+    "url": "/javits-clicky-game/static/media/elena-3rd.9326e9c8.jpg"
+  },
+  {
+    "revision": "74c929a48db85da841596811c5f49fc2",
+    "url": "/javits-clicky-game/static/media/gill-3rd.74c929a4.jpg"
+  },
+  {
+    "revision": "3a102b2f4994da59ecc616d4ee863bf6",
+    "url": "/javits-clicky-game/static/media/hugo-3rd.3a102b2f.jpg"
+  },
+  {
+    "revision": "64e4be92d9b3cd81f1e38d90c4808456",
+    "url": "/javits-clicky-game/static/media/ibuki-3rd.64e4be92.jpg"
+  },
+  {
+    "revision": "60ceb93ac6c139568efcbef8c34031a3",
+    "url": "/javits-clicky-game/static/media/ken-3rd.60ceb93a.jpg"
+  },
+  {
+    "revision": "eb8ac2381d4fe26a20ca7a0eed691e3c",
+    "url": "/javits-clicky-game/static/media/makoto-3rd.eb8ac238.jpg"
+  },
+  {
+    "revision": "3a05a8169b06c9356bc26da47af8829e",
+    "url": "/javits-clicky-game/static/media/remy-3rd.3a05a816.jpg"
+  },
+  {
+    "revision": "37d78445625a6c74b37ff9df36144ce0",
+    "url": "/javits-clicky-game/static/media/ryu-3rd.37d78445.jpg"
+  },
+  {
+    "revision": "66e5499d6f304c2874df06a8bf15611e",
+    "url": "/javits-clicky-game/static/media/sean-3rd.66e5499d.jpg"
+  },
+  {
+    "revision": "d0848d630250e7f9fc12fc089f8432fa",
+    "url": "/javits-clicky-game/static/media/urien-3rd.d0848d63.jpg"
+  },
+  {
+    "revision": "9286a7fdd8d91efa6f68f3e333f9f7c4",
+    "url": "/javits-clicky-game/static/media/yang-3rd.9286a7fd.jpg"
+  },
+  {
+    "revision": "d5eff9b203888e36b58919fff648eb6a",
+    "url": "/javits-clicky-game/static/media/yun-3rd.d5eff9b2.jpg"
+  }
+]);
